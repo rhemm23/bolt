8db2ba11ee003a074c7119b08c1cd529069b6c4d
Ryan Hemmila <rhemmila@wisc.edu>
2020-06-29 12:21:18 -0500

    Added test for null object value

--- Test/TestJson.cs
+++ Test/TestJson.cs
@@ -8,9 +8,11 @@ namespace Test {
   public class TestJson {
     [TestMethod]
     public void TestDeserialization() {
-      string json = "  { \"test_key\" \n : [ null,  \"test_value\" ] } ";
+      string json = "  { \"test_key\" \n : [ null,  \"test_value\" ] , \"test_key_1\" : null } ";
+
       TestJsonObject obj = Json.Deserialize<TestJsonObject>(json);
       Assert.IsTrue(obj.Property.SequenceEqual(new string[] { null, "test_value" }));
+      Assert.IsNull(obj.Property1);
     }
   }
 
@@ -18,5 +20,8 @@ namespace Test {
   public class TestJsonObject {
     [JsonProperty("test_key")]
     public string[] Property { get; set; }
+
+    [JsonProperty("test_key_1")]
+    public string Property1 { get; set; }
   }
 }
