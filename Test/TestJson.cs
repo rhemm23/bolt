db5a4d00603cfacb1664d1195c54396cd0095fa5
Ryan Hemmila <rhemmila@wisc.edu>
2020-06-29 11:25:47 -0500

    Added deserialization test, need to handle array sizes

---
+++ Test/TestJson.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bolt.Attributes;
+using System.Linq;
+using Bolt;
+
+namespace Test {
+  [TestClass]
+  public class TestJson {
+    [TestMethod]
+    public void TestDeserialization() {
+      string json = "  { \"test_key\" \n : [ null,  \"test_value\" ] } ";
+      TestJsonObject obj = Json.Deserialize<TestJsonObject>(json);
+      Assert.IsTrue(obj.Property.SequenceEqual(new string[] { null, "test_value" }));
+    }
+  }
+
+  [JsonObject]
+  public class TestJsonObject {
+    [JsonProperty("test_key")]
+    public string[] Property { get; set; }
+  }
+}
